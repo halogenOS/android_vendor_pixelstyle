2341111bdeb9be1c9eac17d0d657f4f2c346683a
Simão Gomes Viana <devel@superboring.dev>
2021-02-14 13:23:16 +0100

    Checkout and modify stripped down pixel style
    These files were taken from the XOS-10.0 branch.
    All commits are there, this is a stripped down version
    with a few modifications to save bandwidth and space.

---
+++ config.mk
@@ -0,0 +1,30 @@
+# Copyright (C) 2017 The Pure Nexus Project
+# Copyright (C) 2019,2021 The halogenOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := vendor/pixelstyle
+
+# Files
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/etc,$(TARGET_COPY_OUT_PRODUCT)/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts)
+
+# SetupWizard
+PRODUCT_PRODUCT_PROPERTIES += \
+    setupwizard.feature.baseline_setupwizard_enabled=true \
+    setupwizard.theme=glif_v3_light
+
+# Include package overlays
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay/common/
